be52680bb04a329c10cd6a5718604094edf90186
Enn4ji <hamzasholo8@gmail.com>
2026-05-25 15:30:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,5 +26,5 @@ module.exports = {
     DJ_ROLE_NAME: process.env.DJ_ROLE_NAME || 'DJ',
     
     // Cooldown pour les commandes
-    PADD_COOLDOWN_SECONDS: process.env.PADD_COOLDOWN_SECONDS || 5
+    PADD_COOLDOWN_SECONDS: process.env.PADD_COOLDOWN_SECONDS || 0
 };
